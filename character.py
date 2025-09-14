973b2a28ebeb40782e42b7b3f7698f3581eb8f58
Antonio Betancourt <abetancourtt@unal.edu.co>
2025-09-14 13:46:54 -0500

    Ejemplos tarea

--- character.py
+++ character.py
@@ -6,6 +6,35 @@ var = "nombre"
 print(var,type(var))
 print(len(var))
 
+# Autor: Antonio Betancourt
+# Ejemplo Metodo de extracción
+
+primera = var[0]          
+        
+ultima = var[-1]          
+
+print(f"Palabra: {var}\n")
+
+print(f"Primera letra: {primera}\n")
+
+print(f"Última letra: {ultima}\n")
+
+
+# Ejemplo con lower() y ljust()
+
+texto = "HOLA MUNDO"
+print("Texto en mayusculas: ", texto, "\n")
+
+# lower(): convierte todo el texto en minúsculas
+
+texto_en_minusculas = texto.lower()
+print("Texto en minúsculas: ", texto_en_minusculas, "\n")
+
+#ljust(): alinea el texto a la izquierda y rellena con un carácter
+
+texto_justificado = texto.ljust(20, "-")
+print("Texto justificado a la izquierda: ", texto_justificado, "\n")
+
 
 """
 while(True):
