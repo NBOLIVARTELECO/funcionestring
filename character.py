b7cd843359972f507b2d18f810a9a4b05546fb70
Miguel Galindo <migalindor@unal.edu.co>
2025-09-14 23:58:46 -0500

    Agregue funciones Index y Isalnum

--- character.py
+++ character.py
@@ -19,3 +19,12 @@ while(True):
     
 """
 
+#Funcion Index
+LetrasDelAlfabeto = " abcdefghijklmnñopqrstuvwxyz"
+x=input("Ingresa una letra en minuscula del alfabeto para conocer su lugar en el alfabeto: ")
+print("")
+print(LetrasDelAlfabeto.index(x))
+#Funcion Isalnum
+y=input("Ingresa una palabra o frase para verificar si solo cuenta con numeros y/o letras: ")
+print("")
+print(y.isalnum())
